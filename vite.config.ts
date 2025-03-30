259df6ebf008f0fdc24b5747810803ff4f95a845
Zach Puckett <zachpuckett@gmail.com>
2025-03-29 21:38:47 -0700

    update to fix github pages load issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    nodePolyfills({
-      protocolImports: true,
-    }),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-  server: {
-    port: 3000
-  },
-  build: {
-    outDir: 'dist'
-  }
+  base: './',
 })
